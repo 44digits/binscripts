17e22dc59ddc34a23e3c9324cc47fc40d8b4fb0e
Andrew Ross <54513661+44digits@users.noreply.github.com>
2021-06-15 22:04:54 -0700

    updated

--- admin/config/firefox/syspref.js
+++ admin/config/firefox/syspref.js
@@ -1,14 +1,14 @@
-//Enable policies.json
-//https://github.com/pyllyukko/user.js
-//https://gist.github.com/gamunu/7fbee4e2318fdc080395a7f74cc34fe9
-//https://stackoverflow.com/questions/28410049/how-to-disable-firefox-promt-firefox-automatically-sends-some-data-to-mozilla-s
-//https://gist.github.com/0XDE57/fbd302cef7693e62c769
+//Global firefox settings
+//
+// sources:
+//  https://github.com/pyllyukko/user.js
+//  https://gist.github.com/gamunu/7fbee4e2318fdc080395a7f74cc34fe9
+//  https://stackoverflow.com/questions/28410049/how-to-disable-firefox-promt-firefox-automatically-sends-some-data-to-mozilla-s
+//  https://gist.github.com/0XDE57/fbd302cef7693e62c769
 //
 
 //pref("dom.event.clipboardevents.enabled", false);
 //pref("places.history.enabled", false);
-
-
 
 pref("browser.aboutHomeSnippets.updateUrL", "");
 pref("browser.newtabpage.activity-stream.feeds.section.highlights", false);
@@ -37,10 +37,10 @@ pref("browser.safebrowsing.phishing.enabled", false);
 pref("browser.selfsupport.url", "");
 pref("browser.send_pings", false);
 pref("browser.send_pings.require_same_host", true);
-//pref("browser.startup.homepage", "about:blank");
-pref("browser.startup.page", 0);
+pref("browser.startup.homepage", "/etc/firefox/pref/bookmarks.html");
 pref("browser.startup.homepage_override.buildID", "");
 pref("browser.startup.homepage_override.mstone", "ignore");
+pref("browser.startup.page", 0);
 pref("browser.tabs.warnOnClose", false);
 pref("browser.tabs.warnOnCloseOtherTabs", false);
 pref("browser.urlbar.suggest.topsites", false);
@@ -49,7 +49,6 @@ pref("datareporting.healthreport.service.firstRun", false);
 pref("datareporting.healthreport.uploadEnabled", false);
 pref("datareporting.policy.dataSubmissionEnabled", false);
 pref("datareporting.policy.firstRunURL", false);
-pref("datareporting.sessions.current.clean", true);
 pref("datareporting.sessions.current.clean", true);
 pref("devtools.onboarding.telemetry.logged", false);
 pref("dom.battery.enabled", false);
@@ -71,11 +70,11 @@ pref("plugins.enumerable_names", "");
 pref("privacy.clearOnShutdown.cache", true);
 pref("privacy.clearOnShutdown.cookies", true);
 pref("privacy.clearOnShutdown.downloads", false);
-pref("privacy.clearOnShutdown.formData", true);
+pref("privacy.clearOnShutdown.formData", false);
 pref("privacy.clearOnShutdown.history", true);
 pref("privacy.clearOnShutdown.offlineApps", true);
 pref("privacy.clearOnShutdown.sessions", true);
-pref("privacy.clearOnShutdown.siteSettings", true);
+pref("privacy.clearOnShutdown.siteSettings", false);
 pref("privacy.history.custom", true);
 pref("privacy.sanitize.sanitizeOnShutdown", true);
 pref("privacy.trackingprotection.enabled", true);
@@ -100,3 +99,5 @@ pref("toolkit.telemetry.reportingpolicy.firstRun", false);
 pref("toolkit.telemetry.shutdownPingSender.enabled", false);
 pref("toolkit.telemetry.unified", false);
 pref("toolkit.telemetry.updatePing.enabled", false);
+
+
